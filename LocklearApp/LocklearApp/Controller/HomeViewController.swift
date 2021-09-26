6652488165f63dc2448b6b1b26714351f80072e0
Adam <Adam.Mkhtr@hotmail.com>
2021-09-26 19:59:34 +0200

    tab bar has been created

--- LocklearApp/LocklearApp/Controller/HomeViewController.swift
+++ LocklearApp/LocklearApp/Controller/HomeViewController.swift
@@ -17,30 +17,80 @@ class HomeViewController: UIViewController {
   var didTapNotification: (() -> Void)?
 
   //----------------------------------------------------------------------------
+  // MARK: - Outlets
+  //----------------------------------------------------------------------------
+  @IBOutlet weak var streamBarView: UIView!
+  @IBOutlet weak var videoBarView: UIView!
+  @IBOutlet weak var instagramBarView: UIView!
+  @IBOutlet weak var songBarView: UIView!
+
+  //----------------------------------------------------------------------------
   // MARK: - Inits
   //----------------------------------------------------------------------------
 
     override func viewDidLoad() {
         super.viewDidLoad()
+      setupTapGestureRecognizer()
     }
 
+  func setupTapGestureRecognizer() {
+    let tapGestureStream = UITapGestureRecognizer()
+    self.streamBarView.addGestureRecognizer(tapGestureStream)
+    tapGestureStream.addTarget(self, action: #selector(tapStream))
+
+    let tapGestureVideo = UITapGestureRecognizer()
+    self.videoBarView.addGestureRecognizer(tapGestureVideo)
+    tapGestureVideo.addTarget(self, action: #selector(tapVideo))
+
+    let tapGestureInstagram = UITapGestureRecognizer()
+    self.instagramBarView.addGestureRecognizer(tapGestureInstagram)
+    tapGestureInstagram.addTarget(self, action: #selector(tapInstagram))
+
+    let tapGestureSong = UITapGestureRecognizer()
+    self.songBarView.addGestureRecognizer(tapGestureSong)
+    tapGestureSong.addTarget(self, action: #selector(tapSong))
+  }
+
+
   //----------------------------------------------------------------------------
-  // MARK: - Outlets
+  // MARK: - Action
   //----------------------------------------------------------------------------
 
-  @IBOutlet weak var StreamButton: UITabBarItem!
 
-  @IBAction func menuButton(_ sender: Any) {
+  @IBAction func menuButtonTapped(_ sender: Any) {
     didTapMenu?()
   }
 
-  @IBAction func notificationButton(_ sender: Any) {
+  @IBAction func notificationButtonTapped(_ sender: Any) {
     didTapNotification?()
+  }
+
+
+  //----------------------------------------------------------------------------
+  // MARK: - OBJC Mehtods
+  //----------------------------------------------------------------------------
+
+
+  @objc func tapStream() {
+    print("HELLO STREAM")
+  }
+
+  @objc func tapVideo() {
+    print("HELLO Video")
+  }
+
+  @objc func tapInstagram() {
+    print("HELLO Insta")
+  }
+
+  @objc func tapSong() {
+    print("HELLO Song")
   }
 
   //----------------------------------------------------------------------------
   // MARK: - Methods
   //----------------------------------------------------------------------------
+
 
 
 }

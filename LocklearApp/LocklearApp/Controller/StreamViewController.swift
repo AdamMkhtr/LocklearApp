5a5046f0825b27ce1e356c29fb447766d36d1e0c
Adam <Adam.Mkhtr@hotmail.com>
2021-11-28 19:01:41 +0100

    timer countdown in stream view controller has been created

--- LocklearApp/LocklearApp/Controller/StreamViewController.swift
+++ LocklearApp/LocklearApp/Controller/StreamViewController.swift
@@ -9,21 +9,51 @@ import UIKit
 
 class StreamViewController: UIViewController {
 
+
+  //----------------------------------------------------------------------------
+  // MARK: - Properties
+  //----------------------------------------------------------------------------
+
+  var releaseDate: NSDate?
+  var countdownTimer = Timer()
+
+  //----------------------------------------------------------------------------
+  // MARK: - Outlets
+  //----------------------------------------------------------------------------
+
+  @IBOutlet weak var timerLabel: UILabel!
+
+  //----------------------------------------------------------------------------
+  // MARK: - Init
+  //----------------------------------------------------------------------------
   override func viewDidLoad() {
-      super.viewDidLoad()
-
-      // Do any additional setup after loading the view.
+    super.viewDidLoad()
+    startTimer()
   }
 
+  //----------------------------------------------------------------------------
+  // MARK: - Methods
+  //----------------------------------------------------------------------------
 
-  /*
-  // MARK: - Navigation
+  func startTimer() {
 
-  // In a storyboard-based application, you will often want to do a little preparation before navigation
-  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-      // Get the new view controller using segue.destination.
-      // Pass the selected object to the new view controller.
+      let releaseDateString = "2021-11-29 08:00:00"
+      let releaseDateFormatter = DateFormatter()
+      releaseDateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+      releaseDate = releaseDateFormatter.date(from: releaseDateString)! as NSDate
+
+      countdownTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
   }
-  */
+
+  @objc func updateTime() {
+
+      let currentDate = Date()
+      let calendar = Calendar.current
+
+      let diffDateComponents = calendar.dateComponents([.day, .hour, .minute, .second], from: currentDate, to: releaseDate! as Date)
+
+      let countdown = " \(diffDateComponents.day ?? 0): \(diffDateComponents.hour ?? 0): \(diffDateComponents.minute ?? 0): \(diffDateComponents.second ?? 0)"
+    timerLabel.text = countdown
+  }
 
 }
